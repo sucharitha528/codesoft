eef0bb6a7fcd7fd7618755b6dd20d473ea4ba1db
sucharitha528 <158685222+sucharitha528@users.noreply.github.com>
2024-03-29 18:28:31 +0530

    Add files via upload

---
+++ codsoft/Random password.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+# Example: Generate a random password of length 12
+password = generate_password(12)
+print("Random Password:", password)
